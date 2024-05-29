52918e300e73f5cb6d1313edbbb6329102c9a251
Max Shen <m4xshen@gmail.com>
2024-05-29 17:16:06 +0800

    fix: status and priority filter

--- frontend/src/components/Filter/index.tsx
+++ frontend/src/components/Filter/index.tsx
@@ -1,46 +1,53 @@
 'use client';
 
 import { Select, SelectItem } from '@nextui-org/react';
-import { FilterProps } from '@/types';
 import { priority, status } from './data';
 
-const Filter: React.FC<FilterProps> = ({
+function Filter({
   onStatusChange,
   onPriorityChange,
-}) => {
+}: {
+  onStatusChange: (status: boolean | undefined) => void;
+  onPriorityChange: (priority: number | undefined) => void;
+}) {
   return (
     <div className="flex w-full flex-wrap gap-4 md:flex-nowrap">
-      <div className="filter-priority"></div>
       <Select
         label="優先序"
         className="max-w-xs"
         variant="bordered"
-        onSelectionChange={(keys) => {
-          const key = Array.from(keys)[0] as string;
-          const value = key === '' ? 0 : Number(key);
-          onPriorityChange(value);
+        onChange={(e) => {
+          if (e.target.value === '') {
+            onPriorityChange(undefined);
+            return;
+          }
+
+          onPriorityChange(Number(e.target.value));
         }}
       >
-        {priority.map((priority) => (
-          <SelectItem key={priority.key}>{priority.label}</SelectItem>
+        {priority.map((p) => (
+          <SelectItem key={p.key}>{p.label}</SelectItem>
         ))}
       </Select>
       <Select
         label="狀態"
         className="max-w-xs"
         variant="bordered"
-        onSelectionChange={(keys) => {
-          const key = Array.from(keys)[0] as string;
-          const value = key === '' ? 0 : Number(key);
-          onStatusChange(value);
+        onChange={(e) => {
+          if (e.target.value === '') {
+            onStatusChange(undefined);
+            return;
+          }
+
+          onStatusChange(e.target.value === '1');
         }}
       >
-        {status.map((status) => (
-          <SelectItem key={status.key}>{status.label}</SelectItem>
+        {status.map((s) => (
+          <SelectItem key={s.key}>{s.label}</SelectItem>
         ))}
       </Select>
     </div>
   );
-};
+}
 
 export default Filter;
