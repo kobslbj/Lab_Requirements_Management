cebbe26d30ac46e7bb9a7daca44268a6645e6d39
stanleyshen2003 <stanley.shen2003@gmail.com>
2024-05-29 19:51:59 +0800

    register and login (#15)
    * add login and register
    
    * modify schema (lab name)

--- backend/src/services/lab_staff.js
+++ backend/src/services/lab_staff.js
@@ -1,4 +1,7 @@
 const Staff = require("../models/lab_staff.js");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const secretKey = 'secretkey';
 
 const getStaffs = async (req, res) => {
   try {
@@ -21,6 +24,8 @@ const getStaff = async (req, res) => {
 
 const createStaff = async (req, res) => {
   try {
+    // convert password into hash
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
     const staff = await Staff.create(req.body);
     res.status(200).json(staff);
   } catch (error) {
@@ -44,9 +49,39 @@ const deleteStaff = async (req, res) => {
   }
 };
 
+const signinStaff = async (req, res) => {
+  try {
+    const { name, password } = req.body;
+    // Find staff by name
+    const staff = await Staff.findOne({ name });
+    if (!staff) {
+      return res.status(401).json({ message: "Invalid username" });
+    }
+    const isMatch = bcrypt.compareSync(password, staff.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    
+    // Generate JWT
+    const token = jwt.sign(
+      { name: staff.name, id: staff._id, lab_name: staff.lab_name },
+      secretKey,
+      { expiresIn: '1h' }
+    );
+
+    return res.status(200).json({ token });
+  } catch (err) {
+    console.error("Error signing in staff", err);
+    // console.error(err);
+    return res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
     getStaffs,
     getStaff,
     createStaff,
-    deleteStaff
+    deleteStaff,
+    signinStaff,
 };
