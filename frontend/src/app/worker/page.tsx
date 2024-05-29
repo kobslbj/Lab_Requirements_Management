28b9bff41a56fca4cb59bd0419bd2315ce590e0f
Max Shen <m4xshen@gmail.com>
2024-05-29 17:23:21 +0800

    fix: ui layout

--- frontend/src/app/worker/page.tsx
+++ frontend/src/app/worker/page.tsx
@@ -1,20 +1,16 @@
 import TableWithModal from '@/components/TableWithModal';
-
 import { Order } from '@/types';
 
 export default async function AdminPage() {
   const res = await fetch('http://localhost:3000/mock/orders');
   const { data }: { data: Order[] } = await res.json();
   return (
-    <div className="flex h-screen flex-col items-center justify-center p-10">
+    <div className="flex h-screen flex-col items-center justify-center">
       <div className="flex flex-col gap-5">
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold">委託單列表</h1>
         </div>
-        <TableWithModal
-          orders={data}
-          actionType="worker"
-        />
+        <TableWithModal orders={data} actionType="worker" />
       </div>
     </div>
   );
