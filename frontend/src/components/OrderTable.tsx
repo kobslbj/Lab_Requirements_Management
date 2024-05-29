ed733ffb552d4c1a9b12772bf81b3eebb9e36533
Max Shen <m4xshen@gmail.com>
2024-05-29 14:47:19 +0800

    feat: display fab instead of lab in worker page

--- frontend/src/components/OrderTable.tsx
+++ frontend/src/components/OrderTable.tsx
@@ -11,7 +11,6 @@ import {
 import StatusChip from './RowModal/StatusChip';
 import PriorityChip from './RowModal/PriorityChip';
 
-
 export default function OrderTable({
   orders,
   onRowAction,
@@ -21,7 +20,6 @@ export default function OrderTable({
   onRowAction: (id: Key) => void;
   actionType: 'admin' | 'worker';
 }) {
-
   return (
     <Table
       aria-label="Example static collection table"
@@ -35,7 +33,11 @@ export default function OrderTable({
       <TableHeader>
         <TableColumn>標題</TableColumn>
         <TableColumn>品管工程師</TableColumn>
-        <TableColumn>實驗室</TableColumn>
+        {actionType === 'admin' ? (
+          <TableColumn>實驗室</TableColumn>
+        ) : (
+          <TableColumn>廠區</TableColumn>
+        )}
         <TableColumn>優先序</TableColumn>
         <TableColumn>狀態</TableColumn>
       </TableHeader>
@@ -44,7 +46,11 @@ export default function OrderTable({
           <TableRow key={order.id} onClick={() => onRowAction(order.id)}>
             <TableCell>{order.title}</TableCell>
             <TableCell>{order.admin.name}</TableCell>
-            <TableCell>{order.lab?.name}</TableCell>
+            {actionType === 'admin' ? (
+              <TableCell>{order.lab?.name}</TableCell>
+            ) : (
+              <TableCell>{order.fab?.name}</TableCell>
+            )}
             <TableCell>
               <PriorityChip order={order} />
             </TableCell>
