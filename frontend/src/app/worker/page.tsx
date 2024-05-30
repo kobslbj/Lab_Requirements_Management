6d095ed496fb1bbe42ce1a39d716ef1bce7f539b
Max Shen <m4xshen@gmail.com>
2024-05-30 09:53:18 +0800

    refactor: refine worker page

--- frontend/src/app/worker/page.tsx
+++ frontend/src/app/worker/page.tsx
@@ -2,17 +2,17 @@ import TableWithModal from '@/components/TableWithModal';
 import { Order } from '@/types';
 import AvatarButton from '@/components/Avatar';
 
-export default async function AdminPage() {
+export default async function WorkerPage() {
   const res = await fetch('http://localhost:3000/mock/orders');
   const { data }: { data: Order[] } = await res.json();
-  console.log(data)
+
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <div className="flex flex-col gap-5">
         <div className="flex items-center justify-between">
           <div className="text-3xl font-bold">委託單列表</div>
-          {/* 這邊再用props傳入使用者名稱和位置，我先用假資料 */}
-          <AvatarButton name="Justin" position="Lab A"/>
+          {/* TODO: 這邊再用props傳入使用者名稱和位置，我先用假資料 */}
+          <AvatarButton name="Justin" position="Lab A" />
         </div>
         <TableWithModal orders={data} actionType="worker" />
       </div>
