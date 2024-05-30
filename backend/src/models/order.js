a36a4daf2ad43d6954ba93a525815eff135a049d
seanmamasde <seanmamasde@gmail.com>
2024-05-31 05:52:38 +0800

    deez nuts (#22)
    deez nuts

--- backend/src/models/order.js
+++ backend/src/models/order.js
@@ -1,58 +1,37 @@
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+
+const FileSchema = new mongoose.Schema({
+  length: { type: Number, required: true },
+  chunkSize: { type: Number, required: true },
+  uploadDate: { type: Date, required: true },
+  filename: { type: String, required: true },
+  md5: { type: String, required: true },
+  contentType: { type: String, required: true },
+});
 
 const OrderSchema = mongoose.Schema(
   {
-    _id: {
-      type: String,
-      required: true,
-    },
-
-    title: {
-      type: Number,
-      required: true,
-    },
-
-    description: {
-      type: String,
-      required: true,
-    },
-
-    creator: {
-      type: String,
-      required: true,
-    },
-
-    fab_id: {
-      type: String,
-      required: true,
-    },
-
-    lab_id: {
-      type: String,
-      required: true,
-    },
-
-    priority: {
-      type: Number,
-      required: true,
-    },
-
-    is_completed: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-
-    attachment: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "fs.files", // Reference to GridFS files collection
-    },
+    _id: { type: String, required: true },
+    title: { type: Number, required: true },
+    description: { type: String, required: true },
+    creator: { type: String, required: true },
+    fab_id: { type: String, required: true },
+    lab_id: { type: String, required: true },
+    priority: { type: Number, required: true },
+    is_completed: { type: Boolean, required: true, default: false },
+    attachments: [
+      {
+        uuid: { type: String, default: uuidv4 },
+        file: { type: mongoose.Schema.Types.ObjectId, ref: "uploads.files" },
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
 
+const File = mongoose.model("uploads.files", FileSchema);
 const Order = mongoose.model("Order_collection", OrderSchema);
-
-module.exports = Order;
+module.exports = { Order, File };
