1013a0e79dad5be440aaa09a66845aa29aa3e3ba
Justin Li <kobs666666@gmail.com>
2024-06-02 14:45:48 +0800

    feat: Add SubmitButton component for order creation

--- frontend/src/components/OrderCreator/index.tsx
+++ frontend/src/components/OrderCreator/index.tsx
@@ -20,6 +20,7 @@ import { useState, useTransition } from 'react';
 import { createOrder } from '@/actions/order';
 import { Lab, Priority, Upload } from '../Icons';
 import Property from '../RowModal/Property';
+import SubmitButton from './SubmitButton';
 
 export default function OrderCreator() {
   const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
@@ -29,16 +30,9 @@ export default function OrderCreator() {
   const [priority, setPriority] = useState(3);
   const [isPending, startTransition] = useTransition();
   const [file, setFile] = useState<File | null>(null);
-  const fileData = file
-    ? {
-        name: file.name,
-        lastModified: file.lastModified,
-        size: file.size,
-        type: file.type,
-      }
-    : null;
+
   const handleSubmit = async () => {
-    await createOrder(title, description, lab, priority, fileData as File);
+    await createOrder(title, description, lab, priority, file);
     onClose();
     setTitle('');
     setDescription('');
@@ -68,122 +62,129 @@ export default function OrderCreator() {
         onOpenChange={onOpenChange}
       >
         <ModalContent>
-          <ModalHeader className="text-xl">
-            <Input
-              aria-label="委託單名稱"
-              variant="faded"
-              size="lg"
-              radius="sm"
-              placeholder="委託單名稱"
-              value={title}
-              onValueChange={setTitle}
-            />
-          </ModalHeader>
-          <ModalBody>
-            <div className="flex flex-col gap-2 text-sm">
-              <Property
-                name={
-                  <>
-                    <Lab />
-                    實驗室
-                  </>
-                }
-              >
-                <Select
-                  aria-label="實驗室"
-                  variant="faded"
-                  size="sm"
-                  radius="sm"
-                  selectedKeys={[lab]}
-                  onChange={(e) => {
-                    if (e.target.value === '') {
-                      return;
-                    }
-
-                    setLab(e.target.value);
-                  }}
-                >
-                  <SelectItem key="化學實驗室">化學實驗室</SelectItem>
-                  <SelectItem key="表面實驗室">表面實驗室</SelectItem>
-                  <SelectItem key="成分分析實驗室">成分分析實驗室</SelectItem>
-                </Select>
-              </Property>
-              <Property
-                name={
-                  <>
-                    <Priority />
-                    優先序
-                  </>
-                }
-              >
-                <Select
-                  aria-label="優先序"
-                  variant="faded"
-                  size="sm"
-                  radius="sm"
-                  onChange={(e) => {
-                    setPriority(Number(e.target.value));
-                  }}
-                  selectedKeys={[String(priority)]}
-                >
-                  <SelectItem variant="faded" color="danger" key="1" value={1}>
-                    特急單
-                  </SelectItem>
-                  <SelectItem variant="faded" color="warning" key="2" value={2}>
-                    急單
-                  </SelectItem>
-                  <SelectItem variant="faded" color="default" key="3" value={3}>
-                    一般
-                  </SelectItem>
-                </Select>
-              </Property>
-            </div>
-            <Divider />
-            <Textarea
-              aria-label="委託單內容"
-              variant="faded"
-              radius="sm"
-              placeholder="委託單內容"
-              value={description}
-              onValueChange={setDescription}
-            />
-            <Divider />
-            <div className="flex items-center gap-4">
-              <Button
-                radius="sm"
+          <form action={handleSubmit}>
+            <ModalHeader className="text-xl">
+              <Input
+                aria-label="委託單名稱"
+                variant="faded"
                 size="lg"
-                variant="bordered"
-                onPress={() => {
-                  document.getElementById('file-input')?.click();
-                }}
-                className="flex items-center gap-2 rounded border border-gray-300 bg-gray-100 px-4 py-2"
-              >
-                <Upload />
-                <span>選擇檔案</span>
-              </Button>
-              <input
-                id="file-input"
-                type="file"
-                onChange={handleFileChange}
-                accept=".pdf"
-                className="hidden"
+                radius="sm"
+                placeholder="委託單名稱"
+                value={title}
+                onValueChange={setTitle}
               />
-              <span>{file ? file.name : '未選擇任何檔案'}</span>
-            </div>
-          </ModalBody>
-          <ModalFooter>
-            <Button
-              radius="sm"
-              isDisabled={!title || !description}
-              isLoading={isPending}
-              className="bg-black text-white"
-              onPress={() => {
-                startTransition(handleSubmit);
-              }}
-            >
-              新增
-            </Button>
-          </ModalFooter>
+            </ModalHeader>
+            <ModalBody>
+              <div className="flex flex-col gap-2 text-sm">
+                <Property
+                  name={
+                    <>
+                      <Lab />
+                      實驗室
+                    </>
+                  }
+                >
+                  <Select
+                    aria-label="實驗室"
+                    variant="faded"
+                    size="sm"
+                    radius="sm"
+                    selectedKeys={[lab]}
+                    onChange={(e) => {
+                      if (e.target.value === '') {
+                        return;
+                      }
+
+                      setLab(e.target.value);
+                    }}
+                  >
+                    <SelectItem key="化學實驗室">化學實驗室</SelectItem>
+                    <SelectItem key="表面實驗室">表面實驗室</SelectItem>
+                    <SelectItem key="成分分析實驗室">成分分析實驗室</SelectItem>
+                  </Select>
+                </Property>
+                <Property
+                  name={
+                    <>
+                      <Priority />
+                      優先序
+                    </>
+                  }
+                >
+                  <Select
+                    aria-label="優先序"
+                    variant="faded"
+                    size="sm"
+                    radius="sm"
+                    onChange={(e) => {
+                      setPriority(Number(e.target.value));
+                    }}
+                    selectedKeys={[String(priority)]}
+                  >
+                    <SelectItem
+                      variant="faded"
+                      color="danger"
+                      key="1"
+                      value={1}
+                    >
+                      特急單
+                    </SelectItem>
+                    <SelectItem
+                      variant="faded"
+                      color="warning"
+                      key="2"
+                      value={2}
+                    >
+                      急單
+                    </SelectItem>
+                    <SelectItem
+                      variant="faded"
+                      color="default"
+                      key="3"
+                      value={3}
+                    >
+                      一般
+                    </SelectItem>
+                  </Select>
+                </Property>
+              </div>
+              <Divider />
+              <Textarea
+                aria-label="委託單內容"
+                variant="faded"
+                radius="sm"
+                placeholder="委託單內容"
+                value={description}
+                onValueChange={setDescription}
+              />
+              <Divider />
+              <div className="flex items-center gap-4">
+                <Button
+                  radius="sm"
+                  size="lg"
+                  variant="bordered"
+                  onPress={() => {
+                    document.getElementById('file-input')?.click();
+                  }}
+                  className="flex items-center gap-2 rounded border border-gray-300 bg-gray-100 px-4 py-2"
+                >
+                  <Upload />
+                  <span>選擇檔案</span>
+                </Button>
+                <input
+                  id="file-input"
+                  type="file"
+                  onChange={handleFileChange}
+                  accept=".pdf"
+                  className="hidden"
+                />
+                <span>{file ? file.name : '未選擇任何檔案'}</span>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <SubmitButton />
+            </ModalFooter>
+          </form>
         </ModalContent>
       </Modal>
     </>
