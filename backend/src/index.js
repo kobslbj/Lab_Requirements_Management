531f3815c6d32f6f1727cc1d2bbced11763f20ca
stanleyshen2003 <stanley.shen2003@gmail.com>
2024-06-04 04:04:35 +0800

    add docker compose

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,12 @@ const orderRoute = require("./routes/order.js");
 const staffRoute = require("./routes/staff.js");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
